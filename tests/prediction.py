207d37e75d616f3921d1db4325fe7f6910bf86f7
wenlipan <wenli.pan@estudiantat.upc.edu>
2023-10-12 23:15:02 +0200

    Model prediction script

---
+++ tests/prediction.py
@@ -0,0 +1,22 @@
+import torch
+from typing import Tuple, List
+import torchvision.transforms as transforms
+from torchvision.transforms.functional import to_pil_image
+
+
+def predict_image(
+        torch_image: torch.tensor, 
+        model: torch.nn.Module, 
+        topk: int = 1,
+        ) -> List[Tuple[str, float]]:
+    
+    x = torch_image.to(device) # move image to GPU
+    x = x.unsqueeze(0) # add batch dimension
+    
+    # TODO: predict raw outputs
+    output = model(x)
+
+    output = torch.softmax(output, dim=1)  #Compute the softmax to get probabilities
+    probs, idxs = output.topk(topk)  # Get the top k predicitons
+    #return [(labels[i.item()], p.item()*100) for p, i in zip(probs[0], idxs[0])]
+    return probs[0], idxs[0]
