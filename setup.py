b9255e9fcdcf0f793c530f1928b0bcd6ef47aaaf
Alexandra González <gonzalezalvarezalexandra@gmail.com>
2023-10-21 19:29:21 +0200

    setup pylint

--- setup.py
+++ setup.py
@@ -1,10 +1,25 @@
+"""
+Setup script for the Food Image Classification project.
+
+This script is used to configure and install the 'src' package. 
+The project is a part of the Advanced Data Science Topics II course 
+at Universitat Politècnica de Catalunya (UPC) and focuses on 
+classifying food images using deep learning.
+
+Authors: Alexandra González, Wenli Pan, and Violeta Sànchez
+License: MIT
+"""
+
 from setuptools import find_packages, setup
 
 setup(
     name='src',
     packages=find_packages(),
     version='0.1.0',
-    description='This repository contains the project A machine learning project for Advanced Data Science Topics course at Universitat Politècnica de Catalunya (UPC). It . It classifies food images using deep learning.',
+    description="""This repository contains the project
+    for Advanced Data Science Topics II course at Universitat
+    Politècnica de Catalunya (UPC). It classifies food images
+    using deep learning.""",
     author='Alexandra González, Wenli Pan & Violeta Sànchez',
     license='MIT',
 )
