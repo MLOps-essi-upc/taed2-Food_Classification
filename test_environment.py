dd72a3cd6cba8fb50f55c9dd113768a60d00e450
Alexandra González <gonzalezalvarezalexandra@gmail.com>
2023-10-21 19:41:46 +0200

    pylint test_environment

--- test_environment.py
+++ test_environment.py
@@ -1,24 +1,33 @@
+""""
+Check Python Version Compatibility.
+
+This script checks if the Python version matches the 
+required version and raises exceptions if there's a mismatch.
+"""
+
 import sys
 
 REQUIRED_PYTHON = "python3"
 
 
 def main():
+    """Check Python version compatibility."""
+
     system_major = sys.version_info.major
     if REQUIRED_PYTHON == "python":
         required_major = 2
     elif REQUIRED_PYTHON == "python3":
         required_major = 3
     else:
-        raise ValueError("Unrecognized python interpreter: {}".format(
-            REQUIRED_PYTHON))
+        raise ValueError(f"Unrecognized python interpreter: {REQUIRED_PYTHON}")
 
     if system_major != required_major:
         raise TypeError(
-            "This project requires Python {}. Found: Python {}".format(
-                required_major, sys.version))
-    else:
-        print(">>> Development environment passes all tests!")
+            f"This project requires Python {required_major}. "
+            f"Found: Python {sys.version_info.major}"
+        )
+
+    print(">>> Development environment passes all tests!")
 
 
 if __name__ == '__main__':
