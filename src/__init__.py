7f87ad27f6cadcb0a3e597999057378ecb5eeb0d
wenlipan <wenli.pan@estudiantat.upc.edu>
2023-10-22 16:13:35 +0200

    changes

--- src/__init__.py
+++ src/__init__.py
@@ -6,3 +6,8 @@ load_dotenv()
 
 path = os.path.dirname(os.path.abspath("__file__"))
 ROOT_DIR = Path(Path(path).resolve())
+
+RAW_DATA_DIR = ROOT_DIR / "data/raw"
+PROCESSED_DATA_DIR = ROOT_DIR / "data/processed"
+MODELS_DIR = ROOT_DIR / "models"
+METRICS_DIR = ROOT_DIR / "metrics"
