cbd5dc21b300466332cdeed2e9d9d6bfe067b15d
wenlipan <wenli.pan@estudiantat.upc.edu>
2023-10-23 12:26:57 +0200

    update api dictionary

--- src/app/schemas.py
+++ src/app/schemas.py
@@ -7,35 +7,35 @@ class ImageUploadPayload(BaseModel):
     file: UploadFile
     model_conf: dict = {}  # You can specify a default value or leave it as an empty dictionary
 
-    
+
 class FoodClass(str, Enum):
-    beet_salad = 0
-    bibimbap = 1
-    ceviche = 2
-    cheese_plate = 3
-    chicken_curry = 4
-    chicken_quesadilla = 5
-    creme_brulee = 6
-    croque_madame = 7
-    donuts = 8
-    dumplings = 9
-    escargots = 10
-    fish_and_chips = 11
-    french_fries = 12
-    fried_rice = 13
-    frozen_yogurt = 14
-    gnocchi = 15
-    guacamole = 16
-    lasagna = 17
-    lobster_roll_sandwich = 18
-    output = 19
-    pancakes = 20
-    poutine = 21
-    pulled_pork_sandwich = 22
-    ramen = 23
-    red_velvet_cake = 24
-    samosa = 25
-    sashimi = 26
-    seaweed_salad = 27
-    spring_rolls = 28
-    tacos = 29
+    baby_back_ribs = 0
+    beef_carpaccio = 1
+    bibimbap = 2
+    carrot_cake = 3
+    beet_salad = 4
+    cannoli = 5
+    clam_chowder = 6
+    caprese_salad = 7
+    croque_madame = 8
+    bread_pudding = 9
+    crab_cakes = 10
+    cheesecake = 11
+    cup_cakes = 12
+    creme_brulee = 13
+    chocolate_mousse = 14
+    chicken_curry = 15
+    caesar_salad = 16
+    club_sandwich = 17
+    churros = 18
+    oubaklava = 19
+    bruschetta = 20
+    chicken_wings = 21
+    chocolate_cake = 22
+    breakfast_burrito = 23
+    ceviche = 24
+    beef_tartare = 25
+    apple_pie = 26
+    beignets = 27
+    chicken_quesadilla = 28
+    cheese_plate = 29
